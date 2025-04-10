3acb2be8b86ff0da62ab99eb2c3a019a3142b4bf
tahawy111 <amertahawy111@gmail.com>
2025-04-10 17:49:11 +0200

    .

---
+++ lib/presentation/splash/bloc/splash_cubit.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SplashCubit extends Cubit<void> {
+  SplashCubit() : super(null);
+
+  void navigateToNextPage() {
+    // هنا يمكنك إضافة منطق التنقل إلى الصفحة التالية
+    // على سبيل المثال، يمكنك استخدام Navigator.push أو Navigator.pushReplacement
+    // للتنقل إلى صفحة تسجيل الدخول أو الصفحة الرئيسية
+  }
+}
